06d02f8ed0c834683522936589c6df8f060a07a4
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-26 22:51:33 -0300

    Alterado ListView.builder para CustomScrollView com SliverList
    Foi adicionado um título "Destaques" acima da lista.
    A lista de destaques agora é construída usando `CustomScrollView` e `SliverList` com `SliverChildBuilderDelegate`, substituindo o `ListView.builder` anterior.
    O padding foi ajustado para `EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0)`.
    A utilização de Slivers dentro de um CustomScrollView é umas das principais maneiras de evitar overflows de pixel.

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -10,18 +10,29 @@ class Highlights extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return HighlightItem(
-            imageURI: items[index]['image'],
-            itemTitle: items[index]['name'],
-            itemPrice: items[index]['price'],
-            itemDescription: items[index]['description'],
-          );
-        },
-        itemCount: items.length,
-      ),
-    );
+        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: CustomScrollView(slivers: <Widget>[
+          const SliverToBoxAdapter(
+              child: Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              "Destaques",
+              style: TextStyle(
+                fontFamily: "Caveat",
+                fontSize: 32,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => HighlightItem(
+                    imageURI: items[index]['image'],
+                    itemTitle: items[index]['name'],
+                    itemPrice: items[index]['price'],
+                    itemDescription: items[index]['description']),
+                childCount: items.length),
+          )
+        ]));
   }
 }
