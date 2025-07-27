c3729bd23a73dae53bde3f832aee50ddb36bf21f
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-27 11:43:00 -0300

    Tela de bebidas refeita com CustomScrollView e SliverGrid
    A tela de bebidas foi refeita para utilizar `CustomScrollView` e `SliverGrid`, substituindo o `GridView.builder` anterior.
    Um título "Bebidas" foi adicionado acima da grade de bebidas.
    O padding da tela foi ajustado para `EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0)`.

--- lib/screens/drink_menu.dart
+++ lib/screens/drink_menu.dart
@@ -9,15 +9,31 @@ class DrinkMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return DrinkItem(
-              imageURI: items[index]['image'],
-              itemTitle: items[index]['name'],
-              itemPrice: items[index]['price']);
-        },
-        itemCount: items.length);
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: CustomScrollView(slivers: <Widget>[
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text("Bebidas",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "Caveat",
+                    fontSize: 32,
+                  )),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return DrinkItem(
+                imageURI: items[index]['image'],
+                itemTitle: items[index]['name'],
+                itemPrice: items[index]['price'],
+              );
+            }, childCount: items.length),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+          )
+        ]));
   }
 }
