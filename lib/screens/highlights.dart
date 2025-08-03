8a629ea24e935f514ebdf48587aeabb36b3b5590
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-08-03 11:20:28 -0300

    Refatora layout de destaques e bebidas para paisagem
    A lista de destaques agora é exibida em uma grade quando o dispositivo está no modo paisagem.
    A grade de bebidas agora exibe 3 colunas quando o dispositivo está no modo paisagem.

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -5,8 +5,6 @@ import 'package:panucci_ristorante/components/highlight_item.dart';
 class Highlights extends StatelessWidget {
   const Highlights({super.key});
 
-  final List items = destaques;
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -24,15 +22,48 @@ class Highlights extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           )),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, index) => HighlightItem(
-                    imageURI: items[index]['image'],
-                    itemTitle: items[index]['name'],
-                    itemPrice: items[index]['price'],
-                    itemDescription: items[index]['description']),
-                childCount: items.length),
-          )
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? const _landscapeList()
+              : const _portraitList()
         ]));
+  }
+}
+
+class _portraitList extends StatelessWidget {
+  const _portraitList();
+
+  final List items = destaques;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return HighlightItem(
+            imageURI: items[index]['image'],
+            itemTitle: items[index]['name'],
+            itemPrice: items[index]['price'],
+            itemDescription: items[index]['description']);
+      }, childCount: items.length),
+    );
+  }
+}
+
+class _landscapeList extends StatelessWidget {
+  const _landscapeList();
+
+  final List items = destaques;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+        delegate: SliverChildBuilderDelegate((context, index) {
+          return HighlightItem(
+              imageURI: items[index]['image'],
+              itemTitle: items[index]['name'],
+              itemPrice: items[index]['price'],
+              itemDescription: items[index]['description']);
+        }, childCount: items.length),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8, childAspectRatio: 1.4));
   }
 }
